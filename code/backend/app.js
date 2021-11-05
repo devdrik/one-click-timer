fc480d02617197bdcf6202402a7f4370785fd392
Hendrik Ohrdes <devdrik@gmail.com>
2021-11-05 22:47:38 +0000

    added timespan calculation

--- code/backend/app.js
+++ code/backend/app.js
@@ -3,20 +3,26 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 8085;
 
-let state = 'off';
+const STATE = {
+  on: 'on',
+  off: 'off'
+}
+let state = STATE.off;
 const timeList = [];
  
 app.get('/toggle', (req, res) => {
   handleToggleRequest();
   res.send(state)
 });
-
-app.listen(port, () =>
-  console.log(`OneClickTimer backend up on port ${port}!`),
-);
+ 
+app.get('/ontime', (req, res) => {
+  const time = getOnTime();
+  const timeString = getTimeStringFromMs(time);
+  res.send(timeString);
+});
 
 const handleToggleRequest = () => {
-  state = state == 'on' ? 'off' : 'on';
+  state = state == STATE.on ? STATE.off : STATE.on;
   const entry = {
     time: new Date(),
     state
@@ -25,3 +31,28 @@ const handleToggleRequest = () => {
   console.log(entry)
 }
 
+const getOnTime = () => {
+  let passedTimeInMs = 0;
+  for(let i=0; i < timeList.length -1; i=i+2) {
+    passedTimeInMs += timeList[i+1].time - timeList[i].time;
+  } 
+  if(state == STATE.on) {
+    passedTimeInMs += new Date() - timeList[timeList.length - 1].time;
+  }
+  return passedTimeInMs;
+}
+
+const getTimeStringFromMs = timeInMs => {
+  var ms = timeInMs % 1000;
+  timeInMs = (timeInMs - ms) / 1000;
+  var secs = timeInMs % 60;
+  timeInMs = (timeInMs - secs) / 60;
+  var mins = timeInMs % 60;
+  var hrs = (timeInMs - mins) / 60;
+
+  return hrs + ':' + mins + ':' + secs + '.' + ms;
+}
+
+app.listen(port, () =>
+  console.log(`OneClickTimer backend up on port ${port}!`),
+);
