2f56fee6461e5a527b0fc046c6545b208b20aa68
Hendrik Ohrdes <devdrik@gmail.com>
2021-11-08 23:48:13 +0100

    added duration calculation for one day

--- code/java-backend/one-click-timer/src/main/java/de/devdrik/oneclicktimer/rest/WorkingTimeController.java
+++ code/java-backend/one-click-timer/src/main/java/de/devdrik/oneclicktimer/rest/WorkingTimeController.java
@@ -1,5 +1,8 @@
 package de.devdrik.oneclicktimer.rest;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +23,19 @@ public class WorkingTimeController {
     }
 
     @GetMapping("/toggle")
-    public ResponseEntity<String> toggleTimer () {
+    public ResponseEntity<String> toggleTimer() {
         return new ResponseEntity<>(workingTimeService.toggleTimer(), HttpStatus.OK);
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<Iterable<WorkingTime>> getAll () {
+    public ResponseEntity<Iterable<WorkingTime>> getAll() {
         return new ResponseEntity<>(workingTimeService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/getDuration")
+    public ResponseEntity<Duration> getDuration() {
+        // TODO: use date as a parameter instead of today
+        return new ResponseEntity<>(workingTimeService.getCumulativeWorkingTimeOn(LocalDateTime.now()), HttpStatus.OK);
     }
     
 }
--- code/java-backend/one-click-timer/src/main/java/de/devdrik/oneclicktimer/service/WorkingTimeService.java
+++ code/java-backend/one-click-timer/src/main/java/de/devdrik/oneclicktimer/service/WorkingTimeService.java
@@ -1,5 +1,9 @@
 package de.devdrik.oneclicktimer.service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +33,24 @@ public class WorkingTimeService {
 
     public Iterable<WorkingTime> findAll() {
         return workingTimeRepository.findAll();
+    }
+
+    public Duration getCumulativeWorkingTimeOn(LocalDateTime date) {
+        Iterator<WorkingTime> timeList = workingTimeRepository.findAllBetween(date.toLocalDate().atStartOfDay(), date.toLocalDate().atTime(23, 59, 59)).iterator();
+        Duration duration = Duration.ZERO;
+        LocalDateTime previousTime = null;
+        WorkingTime current = null;
+        while (timeList.hasNext()) {
+            current = timeList.next();
+            if (previousTime != null && State.OFF.equals(current.getState())) {
+                duration = duration.plus(Duration.between(previousTime, current.getCreatedDate()));
+            }
+            previousTime = current.getCreatedDate();
+            System.out.println(previousTime.toString());
+        }
+        if (current != null && State.ON.equals(current.getState())) {
+            duration = duration.plus(Duration.between(current.getCreatedDate(), LocalDateTime.now()));
+        }
+        return duration;
     }
 }
