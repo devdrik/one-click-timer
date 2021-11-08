ccc07f49d00465076668f983377765573d031ddd
Hendrik Ohrdes <devdrik@gmail.com>
2021-11-08 18:56:54 +0000

    initialized spring backend

---
+++ code/java-backend/one-click-timer/src/main/java/de/devdrik/oneclicktimer/OneClickTimerApplication.java
@@ -0,0 +1,13 @@
+package de.devdrik.oneclicktimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneClickTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneClickTimerApplication.class, args);
+	}
+
+}
