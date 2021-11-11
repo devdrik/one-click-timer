9f785eb8091b2ceca95dd51e75ad9645dab7feca
Hendrik Ohrdes <devdrik@gmail.com>
2021-11-11 21:21:44 +0100

    switched Table to show start, end and duration in each roe

--- code/frontend/src/components/Table.js
+++ code/frontend/src/components/Table.js
@@ -6,51 +6,94 @@ const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'n
 
 const columns = [
   {
-    field: 'createdDate',
-    headerName: 'Date',
-    minWidth: 200,
+    field: 'ON',
+    headerName: 'From',
+    minWidth: 250,
     flex: 1,
     editable: true,
-    valueFormatter: params => new Date(params.row.createdDate).toLocaleString('en-EN', dateOptions)
+    valueGetter: params => params.value.createdDate,
+    valueFormatter: params => new Date(params.row.ON.createdDate).toLocaleString('en-EN', dateOptions)
   },
   {
-    field: 'state',
-    headerName: 'state',
-    minWidth: 100,
-    flex: 0.2,
+    field: 'OFF',
+    headerName: 'To',
+    minWidth: 250,
+    flex: 1,
+    editable: true,
+    valueGetter: params => params.value.createdDate,
+    valueFormatter: params => new Date(params.row.OFF.createdDate).toLocaleString('en-EN', dateOptions)
+  },
+  {
+    field: 'duration',
+    headerName: 'time',
+    minWidth: 200,
+    flex: 1,
     editable: false,
+    valueFormatter: params => new Date(params.getValue(params.id, 'duration')).toISOString().slice(11,-5)
   },
 ];
 
+let rawWorkingTimes = []
 
 const Table = () => {
 
   const [workingTimes, setWorkingTimes] = useState([]);
 
-  const updateWorkingTimes = params => {
-    console.log(`params`, params)
-    updateWorkingTime(workingTimes
-        .filter(item => item.id === params.id)
-        .map(item => ({...item, createdDate: params.value}))[0])
+  const updateWorkingTimes = (id, newDate) => {
+    updateWorkingTime(rawWorkingTimes
+        .filter(item => item.id === id)
+        .map(item => ({...item, createdDate: newDate}))[0])
       .then(resp => {
-        setWorkingTimes(workingTimes.map(item => item.id === params.id ? resp.data : item))
-        console.log(`resp`, resp)
+        setWorkingTimes(getPreparedData(rawWorkingTimes.map(item => item.id === id ? resp.data : item)))
       })
   }
 
   const handleCellEdit = (params, event) => {
-    console.log(`params`, params)
-    switch(params.field) {
-      case 'createdDate':
-        updateWorkingTimes(params)
+    if (event.type === 'keydown') {
+      event.preventDefault()
+    }
+    let id, date;
+    switch (event.type) {
+      case 'click':
+        id = params.row[params.field].id;
+        break;
+      case 'keydown':
+        id = params.field === 'ON' ? params.id * 2 -1 : params.id * 2;
         break;
       default:
+        id = undefined;
     }
+    date = params.value;
+    updateWorkingTimes(id, date);
+  }
+
+  const getPreparedData = data => {
+    const preparedData = [];
+    let entry = {};
+    let rowId = 1;
+    data.forEach(item => {
+      entry[item.state] = item;
+      if(item.state === 'OFF') {
+        entry.id = rowId;
+        entry.duration = new Date(entry.OFF.createdDate) - new Date(entry.ON.createdDate)
+        preparedData.push(entry);
+        entry = {};
+        rowId++;
+      }
+    })
+    if(data[data.length-1] === 'ON') {
+      entry.id = rowId;
+      entry.duration = new Date() - new Date(entry.ON)
+      preparedData.push(entry);
+    }
+    console.log(`preparedData`, preparedData)
+    return preparedData;
   }
 
   useEffect(() => {
     getAllWorkingTimes().then(resp => {
-      setWorkingTimes(resp.data)
+      rawWorkingTimes = resp.data;
+      setWorkingTimes(getPreparedData(rawWorkingTimes))
     })
   }, [setWorkingTimes])
 
@@ -64,7 +107,6 @@ const Table = () => {
         checkboxSelection={false}
         disableSelectionOnClick
         onCellEditCommit={handleCellEdit}
-
       />
     </div>
   );
