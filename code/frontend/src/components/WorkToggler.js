5078ad36dafef8dd8ed6bddfa07b96ead6f72401
Hendrik Ohrdes <devdrik@gmail.com>
2021-11-11 22:22:33 +0100

    hooked toggle to backend

--- code/frontend/src/components/WorkToggler.js
+++ code/frontend/src/components/WorkToggler.js
@@ -1,18 +1,42 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import PersonOffIcon from '@mui/icons-material/PersonOff';
 import PersonIcon from '@mui/icons-material/Person';
 import ToggleButton from '@mui/material/ToggleButton';
+import { toggleWorkingTime, getWorkingState } from '../services/WorkingTimeService'
 
 const WorkToggler = () => {
   const [selected, setSelected] = React.useState(false);
+
+  const toggle = () => {
+    toggleWorkingTime().then(resp => {
+      let state;
+      switch (resp.data) {
+        case 'on':
+          state = true;
+          break;
+        case 'off':
+          state = false;
+          break;
+        default:
+          alert('we got an error');
+          state = selected;
+      }
+      setSelected(state);
+    })
+  }
+
+  useEffect(() => {
+    getWorkingState().then(
+      resp => setSelected(resp.data === 'on')
+    )
+  }, [setSelected])
 
   return (
     <ToggleButton
       value="check"
       selected={selected}
-      onChange={() => {
-        setSelected(!selected);
-      }}
+      onChange={toggle}
     >
       {selected ? <PersonIcon /> : <PersonOffIcon />}
     </ToggleButton>
--- code/frontend/src/services/WorkingTimeService.js
+++ code/frontend/src/services/WorkingTimeService.js
@@ -2,12 +2,21 @@ import axios from "axios";
 const API_URL = "http://nuc01:8085/";
 const WORKING_TIME_URL = API_URL + "getall";
 const WORKING_TIME_UPDATE_URL = API_URL + "workingtime";
+const WORKING_TIME_TOGGLE_URL = API_URL + "toggle";
+const WORKING_TIME_GET_STATE_URL = API_URL + "state";
 
 export const getAllWorkingTimes = () => {
   return axios.get(WORKING_TIME_URL);
 };
 
 export const updateWorkingTime = (workingTime) => {
-  console.log(`workingTime from Service`, workingTime)
   return axios.put(`${WORKING_TIME_UPDATE_URL}/${workingTime.id}`, workingTime);
 };
+
+export const toggleWorkingTime = () => {
+  return axios.get(WORKING_TIME_TOGGLE_URL);
+}
+
+export const getWorkingState = () => {
+  return axios.get(WORKING_TIME_GET_STATE_URL);
+}
