7fab19ce93350f0d91962fb305e7c697a205838c
Hendrik Ohrdes <devdrik@gmail.com>
2021-12-13 21:03:08 +0100

    changed APIs back to prod

--- code/frontend/src/config/config.js
+++ code/frontend/src/config/config.js
@@ -2,9 +2,8 @@
 
 export const locale = "de-DE";
 export const datePickerformat = "DD.MM.YYYY";
-export const API_URL = "http://nuc01:8081/";
-// export const API_URL = "http://nuc02/oct/api/";
-export const WEBSOCKET_URL =  'http://nuc01:8081/websocket' // window.location.protocol + "//" + window.location.host + "/status";
+export const API_URL = "http://nuc02/oct/api/";
+export const WEBSOCKET_URL =  API_URL + 'websocket';
 
 export const topics = {
     status: '/topic/status'
